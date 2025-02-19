aab4f075a6d8dec2589b0f15f3d1b66256f05ebf
frick <frick@github.com>
2025-02-19 14:31:34 +0100

    Demo register function route

--- src/App.ts
+++ src/App.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import User from './db_models/User';
 
 mongoose.connect("mongodb://127.0.0.1:27017")
 const app = express();
@@ -7,6 +8,22 @@ const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+/**
+ * Register benji as user
+ */
+app.get("/register", async (req, res) => {
+  const newUser = new User({
+    username: "Benji"
+  });
+
+  await newUser.save().then(() => {
+    if(User.findById({username: "Benji"}))
+      res.send("Name already registered");
+    else
+      res.send("Success!");
+  })
 });
 
 app.listen(port, () => {

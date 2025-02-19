85fe9ad4a04a17bf531b93019a1f5fcc1322b492
Your Name <you@example.com>
2025-02-19 15:37:50 +0100

    first try

--- src/db_models/User.ts
+++ src/db_models/User.ts
@@ -1,8 +1,21 @@
 import mongoose from 'mongoose';
 const {Schema, model} = mongoose;
 
+type Entry = {
+    time: Number,
+    typeOfStudy: String,
+    gradeSess: Number,
+    health: Number
+}
+
 const userSchema = new Schema({
     username: String,
+    password: String,
+    mail: String,
+    class: String,
+    activeCourses: Array<String>,
+    pastCourses: Array<String>,
+    sessions: Map <String, Map <String, Entry>>
 });
 
 const User = model("User", userSchema);

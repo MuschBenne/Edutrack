5e79f9aaee03a7540c9f81f34c9673e64e96de64
Your Name <you@example.com>
2025-02-20 14:37:24 +0100

    Fixed register somewhat

--- src/modules/Register.ts
+++ src/modules/Register.ts
@@ -1,12 +1,13 @@
 import User, { Entry } from "../db_models/User";
 
+// fixa lite if saster för vad som kan gå åt HELVETE
 export async function HandleRegister(req, res){
-  const testUsr = "Jakob";
+  const testUsr = JSON.parse(req.body)
   const newUser = new User({
-      username: testUsr,
-      password: "Test",
-      mail: "Test",
-      class: "Test",
+      username: testUsr.user,
+      password: testUsr.pass,
+      mail: testUsr.email,
+      klass: testUsr.klass,
       activeCourses: new Array<String>(),
       pastCourses: new Array<String>(),
       sessions: new Map <String, Map <String, Entry>>()

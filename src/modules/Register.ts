c4dad324e3b4166a8b697aabc79fd121ccdef0f6
frick <frick@github.com>
2025-02-20 16:03:12 +0100

    + POST route for registering a user. + Minor cleanup of Register view and module.

--- src/modules/Register.ts
+++ src/modules/Register.ts
@@ -1,19 +1,20 @@
+import { Request, Response } from "express";
 import User, { Entry } from "../db_models/User";
 
 // fixa lite if saster för vad som kan gå åt HELVETE
-export async function HandleRegister(req, res){
-  const testUsr = JSON.parse(req.body)
-  const newUser = new User({
-      username: testUsr.user,
-      password: testUsr.pass,
-      mail: testUsr.email,
-      klass: testUsr.klass,
-      activeCourses: new Array<String>(),
-      pastCourses: new Array<String>(),
-      sessions: new Map <String, Map <String, Entry>>()
+export async function HandleRegister(req: Request, res: Response){
+    const reqData = req.body;
+    const newUser = new User({
+        username: reqData.user,
+        password: reqData.pass,
+        mail: reqData.email,
+        class: reqData.class,
+        activeCourses: new Array<String>(),
+        pastCourses: new Array<String>(),
+        sessions: new Map <String, Map <String, Entry>>()
     });
-    
+      
     await newUser.save().then(() => {
-          res.send("Success!");
+        res.sendStatus(200);
     })
 }

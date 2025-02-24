8dba1b4fad1c3928f5e2cd00427ae73a420f07a3
Bernhard5505 <bernhardedvinsson@gmail.com>
2025-02-24 16:39:16 +0100

    sketch login.ts

--- src/modules/Login.ts
+++ src/modules/Login.ts
@@ -1,7 +1,21 @@
 import User from "../db_models/User";
+import { Request, Response } from "express";
+import mongoose from "mongoose";
 
-
-export async function HandleLogin(req, res){
+export async function HandleLogin(req:Request, res:Response){
     // TODO: Skapa en funktion för att hantera inloggning
     //       Använd Register.ts som mall
+    
+    const reqData=req.body
+
+    //kolla om user finns
+    const foundUser = await User.findOne({username: reqData.user}).exec();
+    //kolla om password matchar
+    if(foundUser){
+        if(password=foundUser.password){
+            //login
+        }
+    }else
+        console.log("wrong username or password")
+    
 }

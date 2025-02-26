89c3f3db8d1497fd3753c3474c9de038ce4fa9ea
Your Name <you@example.com>
2025-02-26 09:29:45 +0100

    function commits

--- src/modules/Register.ts
+++ src/modules/Register.ts
@@ -31,7 +31,7 @@ export async function HandleRegister(req: Request, res: Response){
         if(!foundUser){
             if (!foundEmail)
                 await newUser.save().then(() => {
-                res.sendStatus(200).json({message: "User created"});
+                res.status(200).json({message: "User created"});
             });
         }
     }

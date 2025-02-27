7a4dd3864c90d70129197156c646ba3ecc70d362
Your Name <you@example.com>
2025-02-27 14:47:36 +0100

    Tog bort / la till kommentarer

--- src/modules/Landing.ts
+++ src/modules/Landing.ts
@@ -1,7 +1,5 @@
 import { Request, Response } from "express";
 
 export async function HandleLanding(req: Request, res: Response){
-    // TODO: Skapa en kul landing page, alltså: vad ska man se när man först går in på hemsidan localhost:3000
-    // Mesta av sidan kan göras i viewen "Landing.ejs"
     res.render("Landing");
 }
--- src/modules/Logout.ts
+++ src/modules/Logout.ts
@@ -1,9 +1,6 @@
 import express, {Response, Request, NextFunction} from "express";
 import { Session } from "express-session";
 export async function HandleLogout(req: Request, res: Response, next: NextFunction){
-  // TODO: Hantera logout, avsluta sessionen som skapas av express-session och skicka användaren till landing page
-  //       Läs på express-session dokumentationen om hur detta görs
-  //       https://github.com/expressjs/session?tab=readme-ov-file#sessionregeneratecallback
   req.session.regenerate(() => {
     res.redirect("/");
     next();
